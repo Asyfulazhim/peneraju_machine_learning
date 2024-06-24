1afeabd6395656ffa16e90a77565a4c4c718f7f7
Asyful <asyfaliq@gmail.com>
2024-06-25 00:17:30 +0800

    Q10

---
+++ python105/Question10.py
@@ -0,0 +1,13 @@
+c = input("Enter characters:")
+char = list(c)
+charlength = len(char)
+print(char)
+
+i = 0
+while i < charlength:
+    count = 1
+    while i + 1 < charlength and char[i] == char[i+1]:
+        i += 1
+        count += 1
+    print(f"{char[i]}{count}", end="")
+    i += 1
