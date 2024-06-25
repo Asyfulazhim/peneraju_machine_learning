d9c2819e36062d45f4fc1c6d43f779cfb674bc68
Asyful <asyfaliq@gmail.com>
2024-06-25 11:34:48 +0800

    passing function into function

--- python/functions.py
+++ python/functions.py
@@ -157,4 +157,29 @@ def listSixLetterFruits(*fruits):
 listSixLetterFruits("Apple",20, 1.40, "Orange", 40, 1.2)
 
 
+# sum function
+def sum(a,b):
+    total = a + b
+    print(total)
+    return total
 
+# minus fucntion
+def minus(a,b):
+    total = a - b
+    print(total)
+    return total
+
+def mul(a,b):
+    total = a * b
+    print(total)
+    return total
+
+# create fucntion that can call function created in future
+#     
+def arithmetic(func, a, b):
+    func(a,b)
+
+
+arithmetic(sum, 10, 20)
+arithmetic(minus, 20, 10)
+arithmetic(mul, 10, 20)
