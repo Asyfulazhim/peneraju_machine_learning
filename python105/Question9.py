6d31d4032f235fa2c2477d56d42cbbd869d6ad28
Asyful <asyfaliq@gmail.com>
2024-06-25 21:58:08 +0800

    convert roman to int complete

--- python105/Question9.py
+++ python105/Question9.py
@@ -25,5 +25,43 @@ roman_dict = {'I': 1, 'V': 5, 'X': 10
               , 'L': 50, 'C': 100, 'D': 500,
               'M': 1000}
 
-print(roman_dict[roman])
-print(roman_dict['X'])
+def value(v):
+    if v == 'I':
+        return roman_dict["I"]
+    elif v == 'V':
+        return roman_dict["V"]
+    elif v == 'X':
+        return roman_dict["X"]
+    elif v == 'L':
+        return roman_dict["L"]
+    elif v == 'C':
+        return roman_dict["C"]
+    elif v == 'D':
+        return roman_dict["D"]
+    elif v == 'M':
+        return roman_dict["M"]
+    else:
+        return 0
+    
+i = 0
+intnum = 0
+while i < len (roman):
+    p1 = value(roman[i])
+    if i + 1 < len(roman):
+        p2 = value(roman[i + 1])
+        if p1 >= p2:
+            intnum += p1
+            i += 1
+        else:
+            intnum += p2 - p1
+            i += 2
+    else:
+        intnum += p1
+        i += 1
+
+print(f"{roman} = {intnum}")
+    
+
+
+# print(roman_dict[roman])
+# print(roman_dict['X'])
