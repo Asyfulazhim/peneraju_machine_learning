75fceacd25a60726ecc0d0393535234af9426cb6
Asyful <asyfaliq@gmail.com>
2024-06-26 12:51:49 +0800

    three methods to import module

--- python/mymoduleclient.py
+++ python/mymoduleclient.py
@@ -1,3 +1,17 @@
-import mymodule
+# First Module
+# import mymodule
 
-print(mymodule.simpleInterest(1000,1,6))
+# print(mymodule.simpleInterest(1000,1,6))
+
+# Second Method
+# python recommend this method
+from mymodule import simpleInterest, compoundInterest
+print(simpleInterest(1000,1,6))
+print(compoundInterest(1000,1,6))
+print("-"* 30)
+
+#Third method
+from mymodule import *
+print(simpleInterest(1000,1,6))
+print(compoundInterest(1000,1,6))
+print("-"* 30)
