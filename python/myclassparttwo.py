5ec08c48aab72013c9b585f71761a27dddc68530
Asyful <asyfaliq@gmail.com>
2024-06-28 12:11:41 +0800

    set values for the properties

--- python/myclassparttwo.py
+++ python/myclassparttwo.py
@@ -18,7 +18,30 @@ class Student:
         grade = "A"
         print(f"Attending Exam: {exam},\nGrade: {grade}")
     
+    def info(self):
+        print("First name:", self.firstname)
+        print("Last name:", self.lastname)
+        print("IC Number:", self.icnumber)
+        print("Program:", self.program)
+        print("Address:")
+        print(self.address["Street"])
+        print(self.address["City"])
+        print(self.address["State"])
+                                                                    
+    
 zul = Student("Ahmad", "Syahmi", "00077667412")
 zul.attendClass()
 zul.doProject("Project 1")
 zul.attendExam("Pyhton103")
+zul.address = {
+    "Street": "15 Jalan s32",
+    "City": "Kuala Lumpur",
+    "State": "Selangor"
+    }
+zul.info()
+
+# how we can set value for the properties
+# 1) using constructor (__init__method)
+# 2) you can set value to the property directly using dot operator
+# eg: zul.program["Python", "Data Science", "Machine Learning"]
+
