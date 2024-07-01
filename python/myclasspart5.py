f8bb5b9e1d7a11de7d73bc24311589037177c18e
Asyful <asyfaliq@gmail.com>
2024-07-01 15:50:28 +0800

    Is - A Relationship

--- python/myclasspart5.py
+++ python/myclasspart5.py
@@ -9,6 +9,22 @@ class Student:
 
 # Alumni extends Student class
 class Alumni(Student):
-    pass
 
-alumni = Alumni("Parker", "Fikri")
+    def __init__(self,firstname, lastname, alumninumber):
+        # static calling the __init__ 
+        # Student.__init__(self,firstname, lastname)
+        # you can use the keyword "super"
+        super().__init__(firstname,lastname)
+        self.alumninumber = alumninumber
+        pass
+
+    def __str__(self):
+        return f"First Name: {self.firstname} \
+        \nLast Name: {self.lastname} \
+        \nIC Number: {self.icnumber} \
+        \nAlumni Number: {self.alumninumber}"
+    
+
+alumni = Alumni("Parker", "Fikri", 970856)
+#alumni = Alumni("","")
+print(alumni)
