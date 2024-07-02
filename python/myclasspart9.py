5f76b89e90c8a322184f74e9685fbe0aeac44a2f
Asyful <asyfaliq@gmail.com>
2024-07-02 16:48:41 +0800

    extra practice

--- python/myclasspart9.py
+++ python/myclasspart9.py
@@ -11,3 +11,26 @@ class Calculator:
     
 mycalculator = Calculator(10,20)
 print(mycalculator.add())
+
+class Utility:
+    def addition(x, y):
+        return x + y
+    
+    def subtraction(x, y):
+        return x - y
+    
+print(Utility.addition(100,200))
+
+class Customer:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def getFullname(first, last):
+        return first + last
+    
+    def __str__(self):
+        return Customer.getFullname(self.first, self.last)
+    
+myCustomer = Customer("John","David")
+print(myCustomer)
